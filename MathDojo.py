e0c40c4b0dd3dcc236155b10307f6ae000fd4f93
carlos arosemena <ceag68@hotmail.com>
2017-10-08 15:09:27 -0400

    math

---
+++ MathDojo.py
@@ -0,0 +1,8 @@
+class md(object):
+    def __init__(self, arg1, *args):
+        return self.add(args) - self.substract(args)
+
+    def add(self, arg1, *args):
+
+
+    def substract(self, arg1, *args):
