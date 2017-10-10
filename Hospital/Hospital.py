013a0d86c4507b014226064fa710050eda0e9e48
carlos arosemena <ceag68@hotmail.com>
2017-10-10 10:17:06 -0400

    hospital

--- Hospital/Hospital.py
+++ Hospital/Hospital.py
@@ -7,19 +7,44 @@ class hospital(object):
         self.hospName = hospName
         self.capacity = capacity
 
+    def patient_count(self):
+        if len(self.patients) >0:
+            for patient in self.patients:
+                print "Patient Id: "+str(patient.patientId)
+                print "Patient Name: "+str(patient.name)
+                print "Patient allergies: "+patient.allergies
+                print "Bed Number: "+str(patient.bedNo)
+                print "======================================"
+
+        return self
+
     def admit_patient(self,name,allergies, *bedNo):
+        if self.capacity > 0:
 
-        if len(name)<1:
-            return "Please insert a name"
+            hospPatient = patient(name,allergies,*bedNo)
+            self.patients.append(hospPatient)
 
-        if len(allergies)<1:
-            return "Please describe allergies, if none, just type 'N/A'"
-
-        if bedNo:
-            for bed in bedNo:
-                self.bedNo = bed
+            self.capacity -= 1
         else:
-            self.bedNo = None
+            print "No more capacity"
 
-        hospPatient = patient(name,allergies,bedNo)
-        
+        return self
+
+    def discharge_patient(self,name):
+
+        if self.patients:
+            for patient in self.patients:
+                if name == patient.name:
+                    self.patients.pop(self.patients.index(patient))
+                    self.capacity += 1
+                else:
+                    print "Patient name not in list"
+        else:
+            print "No patients to remove"
+
+        return self
+
+
+hosp = hospital("arohosp",2)
+
+hosp.admit_patient('gato','gato allergies', 246).admit_patient('Carlos','carlos allergies').discharge_patient('gato').admit_patient('Carl','carl allergies', 248).patient_count()
