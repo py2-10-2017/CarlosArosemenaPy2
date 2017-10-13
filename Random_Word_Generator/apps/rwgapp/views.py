eea0bf42fe66ef74fa8e7352268bff6a08aa0a65
carlos arosemena <ceag68@hotmail.com>
2017-10-13 18:15:16 -0400

    Random

--- Random_Word_Generator/apps/rwgapp/views.py
+++ Random_Word_Generator/apps/rwgapp/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.utils.crypto import get_random_string
 
 def index(req):
+    try:
+        req.session['counter']
+        req.session['randomWord']
+
+    except KeyError:
+        req.session['counter'] = 0
+        req.session['randomWord'] = 'Please click "Generate" to get display a random word"'
+
     context = {
-        "randomWord":"CarlosArosemenaWord"
+        'counter':req.session['counter'],
+        'randomWord':req.session['randomWord']
     }
+    # req.session['randomWord'] = "Click 'Generate' to display a random word"
     return render(req, 'rwgapp/index.html', context)
 
-# Create your views here.
+def generate(req):
+    randomness = get_random_string(length=14)
+    print randomness
+    req.session['randomWord'] = randomness
+    req.session['counter'] += 1
+    print req.session['counter']
+    print req.session['randomWord']
+    return redirect('/')
+
+def reset(req):
+    del req.session['counter']
+    del req.session['randomWord']
+    return redirect('/')
