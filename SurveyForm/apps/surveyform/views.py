c9e0e36968bc18d087e95becde2c2b511341b0c5
carlos arosemena <ceag68@hotmail.com>
2017-10-14 13:33:39 -0400

    handle blanks better

--- SurveyForm/apps/surveyform/views.py
+++ SurveyForm/apps/surveyform/views.py
@@ -3,23 +3,38 @@ from django.shortcuts import render, HttpResponse, redirect
 # Create your views here.
 
 def index(req):
-
-    return render(req, 'surveyform/index.html')
+    context = {
+        'error': req.session['error']
+    }
+    return render(req, 'surveyform/index.html', context)
 
 def result(req):
 
-    
+
     context = {
         'name' : req.session['name'],
         'location': req.session['location'],
         'language' : req.session['language'],
         'comment': req.session['comment'],
-        'counter': req.session['counter']
+        'counter': req.session['counter'],
+
     }
 
     return render(req, 'surveyform/result.html', context)
 
 def process(req):
+    req.session['error'] =''
+
+    try:
+        req.POST['name']
+        req.POST['location']
+        req.POST['language']
+        req.POST['comment']
+    except KeyError:
+        req.session['error'] = "No blank fields"
+        return redirect('/')
+
+
     req.session['counter'] += 1
     req.session['name'] = req.POST['name']
     req.session['location'] = req.POST['location']
