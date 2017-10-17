9d45896966081f323b1a54d70a2fba64e3682aca
carlos arosemena <ceag68@hotmail.com>
2017-10-17 11:40:26 -0400

    booksupdated

--- BooksAuthors/apps/bookapp/models.py
+++ BooksAuthors/apps/bookapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+#Create a new model called 'Book' with the information above.
+#Create a new model called 'Author' with the information above.  Design the models in a way that you could perform the
 
-# Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    desc = models.TextField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    notes = models.TextField()
+    books = models.ManyToManyField(Book, related_name="authors")
--- BooksAuthors/apps/bookapp/views.py
+++ BooksAuthors/apps/bookapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from queries import Bookstore
 
 # Create your views here.
+def index(req):
+    for book in Bookstore:
+        response = book['name']
+    return HttpResponse(response)
